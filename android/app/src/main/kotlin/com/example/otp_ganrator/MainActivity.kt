21174cf6724e0941a3ba68922a028662713dbb87
Ansh <nikunjbhanderi135@gmail.com>
2023-01-07 23:29:08 -0800

    otp

---
+++ android/app/src/main/kotlin/com/example/otp_ganrator/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.otp_ganrator
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:otp_ganrator/HomePage.dart';
+
+void main(){
+  runApp(MaterialApp(home: Home(),),);
+}
